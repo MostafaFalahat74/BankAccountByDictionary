2a467f8116de248bf8815c6b2f06f4377bf11bad
mosi <mosi@DESKTOP-ABD4QVJ>
2021-02-25 23:12:53 -0800

    Add project files.

---
+++ BankAccount/Controller/Operations.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace BankAccount.Controller
+{
+    public class Operations
+    {
+        Dictionary<int  , int > users= new Dictionary<int, int>();      
+
+        public void DeleteAccount(int index)
+        {
+            users.Remove(index);
+
+        }
+        public int ShowInventory(int id)
+        {
+            return users[id];
+        }
+
+        public void DepositMoneyToAccount(int index, int money)
+        {          
+            users[index] += money;
+        }
+        public int CheckAccountExist(int deposit)
+        {
+           
+            int index = users.Where(x => x.Key == deposit).Select(x => x.Key).FirstOrDefault();         
+            return index;
+        }  
+        
+        public void CreateNewUser( int nationalCard , int inventory) {
+
+            users.Add( nationalCard,  inventory);                   
+        }
+        public int GetUser(int index)
+        {
+            return users[index];
+        }
+
+        public bool CheckUserAge(int age)
+        {
+            bool result = true;
+            if (age < 18)
+            {
+                result = false;
+            }
+            return result;
+        }
+    }
+}
---
+++ BankAccount/View/Program.cs
@@ -0,0 +1,154 @@
+﻿using BankAccount.Controller;
+using System;
+
+namespace BankAccount
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            string userName;
+            bool permissionToBuildAccount;
+            Operations operations = new Operations();
+            string userChoice;
+            int accountDeposit;
+            int nationalCard;
+            int inventory;
+            int amountMoneyDeposit;
+            int age;
+
+            while (true)
+            {
+
+                Console.WriteLine("1 : For Create Account  ");
+                Console.WriteLine("2 : For Deposit To Account  ");
+                Console.WriteLine("3 : For Show Inventory  ");
+                Console.WriteLine("4 : For Delete Account  ");
+                userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "1":
+
+                        Console.WriteLine("Plese Enter User Name");
+                        userName = Console.ReadLine();
+
+                        Console.WriteLine("Plese Enter National Card");
+                        nationalCard = Int32.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Plese Enter Age");
+                        age = Int32.Parse(Console.ReadLine());
+
+                        permissionToBuildAccount = operations.CheckUserAge(age);
+                        if (permissionToBuildAccount)
+                        {
+                            Console.WriteLine("Plese Enter Inventory");
+                            inventory = Int32.Parse(Console.ReadLine());
+                             operations.CreateNewUser( nationalCard, inventory);
+                            Console.Clear();
+                            Console.WriteLine($"Account Number : {nationalCard} And inventory : {inventory} ");
+                        }
+                        else
+                        {
+                            Console.WriteLine("  your age has under 18 ");
+                            Console.ReadKey();
+                            Console.Clear();
+                            Console.WriteLine(" please enter father name ");
+                            string fatherName = Console.ReadLine();
+
+
+                            Console.WriteLine(" please enter father age ");
+                            int fatherAge = Int32.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Plese Enter Inventory");
+                            inventory = Int32.Parse(Console.ReadLine());
+                            Console.Clear();
+                            if (fatherAge > 18)
+                            {
+                                 operations.CreateNewUser( nationalCard, inventory);
+                                Console.WriteLine($"Account Number : {nationalCard} And inventory : {inventory} ");
+                            }
+                            else
+                            {
+                                Console.WriteLine(" sorry father age under 18");
+                            }
+                        }
+
+
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("Plese Enter Account to Deposit money");
+                        int accountNumber = Int32.Parse(Console.ReadLine());
+                        int accountIndex = operations.CheckAccountExist(accountNumber);
+                        if (accountIndex != 0)
+                        {
+                            Console.WriteLine("Plese Enter User amount of money to deposit");
+                            amountMoneyDeposit = Int32.Parse(Console.ReadLine());
+                            operations.DepositMoneyToAccount(accountIndex, amountMoneyDeposit);
+                            Console.Clear();
+                            Console.WriteLine($"the amount of {amountMoneyDeposit} to the account {accountIndex} deposited");
+                          
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("The Account Does Not Exist ");
+                        }
+                      
+
+                        break;
+
+                    case "3":
+                        Console.Clear();
+                        Console.WriteLine("Plese Enter Account Number");
+                        accountDeposit = Int32.Parse(Console.ReadLine());
+                        int accountExist = operations.CheckAccountExist(accountDeposit);
+
+                        if (accountExist != 0)
+                        {
+                            Console.Clear();
+                            var inventoryAccount = operations.ShowInventory(accountExist);
+                            Console.WriteLine($" inventor of account {accountDeposit} is {inventoryAccount}");
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("the account is dont exist");
+                        }
+
+
+                        break;
+
+                    case "4":
+                        Console.Clear();
+                        Console.WriteLine("Plese Enter Account to Deposit money");
+                        int account = Int32.Parse(Console.ReadLine());
+                        int accountForDeleteExist = operations.CheckAccountExist(account);
+                        if (accountForDeleteExist != 0)
+                        {
+                            operations.DeleteAccount(accountForDeleteExist);
+
+                            Console.Clear();
+                            Console.WriteLine($"Account {account} deleted");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine(" Account Does Not Exist ");
+                        }
+
+                        break;
+                }
+
+            }
+
+        }
+
+    }
+}
